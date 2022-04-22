d0e365f038fe4ac95e5ed87ed9f5b219169a00e3
Oliver Redfern <oredfern@me.com>
2022-04-22 11:27:06 +0100

    updated homepage content

--- src/components/homepageContent.jsx
+++ src/components/homepageContent.jsx
@@ -4,14 +4,17 @@ import ImageComponent from '../components/imageComponent';
 import TextComponent from '../components/textComponent';
 
 import ImageSnacks from '../images/home-snacks.png';
+import ImageTickets from '../images/home-tickets.png';
 
 const homepageComponent = () => {
   return (
 
     <>
 
- <ImageComponent title="Test text" subtitle="test subtitle" image={ImageSnacks} />
- <TextComponent content="Test text here"  />
+ <ImageComponent image={ImageSnacks} />
+ <TextComponent content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus massa mauris, efficitur in urna eget, aliquam aliquet nisi. Praesent a felis at libero accumsan efficitur. Aenean a justo fringilla libero rhoncus mattis sit amet et dui. Nullam eget urna vitae nisi ultrices lacinia. Etiam vestibulum tristique sem, sed sagittis turpis varius in."  />
+ <ImageComponent image={ImageTickets} />
+ <TextComponent content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus massa mauris, efficitur in urna eget, aliquam aliquet nisi. Praesent a felis at libero accumsan efficitur. Aenean a justo fringilla libero rhoncus mattis sit amet et dui. Nullam eget urna vitae nisi ultrices lacinia. Etiam vestibulum tristique sem, sed sagittis turpis varius in."  />
 
   </>
 
