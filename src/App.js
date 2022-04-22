46e75578ba54b053c49ed4cfaa020ab23e86409f
Oliver Redfern <oredfern@me.com>
2022-04-22 12:09:31 +0100

    Header added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import './App.css';
-import Header from './Header';
+import Header from './components/header';
 import HeroBannerComponent from './HeroBannerComponent';
 import HomepageComponent from './components/homepageContent';
 import homeBanner from './images/HomeBanner.png';
 
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+
 function App() {
   return (
-    <div className="App">
-      <Header />
+    <Router>
+    <Header />
+   
+    <Routes>
+      <Route path="/" element={
+      <>
+      <div className="App">
+     
       <HeroBannerComponent image={homeBanner}/>
  <HomepageComponent />
  
 
     </div>
+   
+      </>
+      }/>
+
+      </Routes>
+      </Router>
+
+   
   );
 }
 
