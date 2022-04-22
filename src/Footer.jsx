adcb45502abba431f87015ae939ea20076ee1daf
Shrestha, Yukta <Yukta.Shrestha@Chubb.com>
2022-04-22 15:41:27 +0100

    Implemented footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import footerLogo from './images/CCC-Logo2.png';
+import map from './images/map.png';
 
 const Footer = () => {
     return (
@@ -7,15 +8,26 @@ const Footer = () => {
             <>
                 <img src={footerLogo}
                     alt="Footer logo"
-                    width={100}
-                    style={{ float: 'left', padding: '0px 0px 10px 10px' }}>
+                    width={150}
+                    style={{ float: 'left', padding: '20px 0px 10px 120px' }}>
                 </img>
-                <ul style={{listStyle: 'none', backgroundColor: '#696969'}}>
-                    <li style={{fontWeight: 'bold', color: 'black'}}>More Info</li>
-                    <li>About us</li>
-                    <li>FAQ</li>
-                    <li>Contact us</li>
+                <ul style={{ listStyle: 'none', backgroundColor: '#696969', padding: '30px 30px 30px 25%', color: 'white' }}>
+                    <li style={{ fontWeight: 'bold' }}><a href="/" style={{ textDecoration: 'none', color: 'white' }}>More Info</a></li><br></br>
+                    <li><a href="/" style={{ textDecoration: 'none', color: 'white' }}>About us</a></li>
+                    <li><a href="/" style={{ textDecoration: 'none', color: 'white' }}>FAQ</a></li>
+                    <li><a href="/" style={{ textDecoration: 'none', color: 'white' }}>Contact us</a></li>
                 </ul>
+                <div>
+                    <img src={map}
+                        alt="Footer logo"
+                        width={150}
+                        style={{ float: 'left', padding: '20px 0px 10px 120px' }}>
+                    </img>
+
+                    </div>
+
+
+                    <h3>Copyright &copy;</h3>
             </>
         </footer>
     );
